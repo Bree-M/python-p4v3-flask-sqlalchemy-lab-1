092ab53bde33d72455b3aec9d1aef38146c977ef
Bree-M <brenda.wairimu@student.moringaschool.com>
2025-06-18 09:14:03 +0300

    push my commit

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, jsonify, make_response
 from flask_migrate import Migrate
 
 from models import db, Earthquake
@@ -21,6 +21,44 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    earthquake = db.session.get(Earthquake, id)
+
+    if not earthquake:
+        return make_response( 
+            jsonify({'message' : f'Earthquake {id} not found.'}),
+            404
+        )
+    return make_response(
+        jsonify({
+            'id': earthquake.id,
+            'location': earthquake.location,
+            'magnitude': earthquake.magnitude,
+            'year' : earthquake.year
+
+    }),
+    200
+)
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquakes_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(
+        Earthquake.magnitude >= magnitude
+    ).all()
+
+    quakes_data = [{
+        'id': eq.id,
+        'location': eq.location,
+        'magnitude': eq.magnitude,
+        'year': eq.year
+
+    } for eq in earthquakes]
+
+    return jsonify({
+        'count': len(earthquakes),
+        'quakes': quakes_data
+    })
 
 
 if __name__ == '__main__':
